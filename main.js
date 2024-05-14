a1eb27e3479b41926d87d181fd2a88662ba522ce
hzy <i@anclx.cc>
2024-05-14 21:45:04 +0800

    update
    Signed-off-by: hzy <i@anclx.cc>

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+const code = document.getElementById('code')
+const width = document.getElementById('width')
+const height = document.getElementById('height')
+const format = document.getElementById('format')
+const fileElement = document.getElementById('file')
+
+fileElement.addEventListener('change', event => {
+    // load & run
+    const file = fileElement.files[0]
+    console.log(file)
+})
